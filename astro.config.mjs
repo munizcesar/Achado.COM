e0412e97796d30725750e92472363c3b0f618e0d
César Muniz <147671167+munizcesar@users.noreply.github.com>
2026-02-27 19:49:16 -0300

    fix: remove sitemap (bug reduce undefined) + trava Astro 4.15.x + wrangler.toml correto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://achadocerto.vip',
-  integrations: [sitemap()],
   output: 'static',
   build: {
     assets: '_assets'
