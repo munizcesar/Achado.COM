e4d54645dbf9563425144769fd8df8673ffa0404
munizcesar@live.com <147671167+munizcesar@users.noreply.github.com>
2026-04-17 21:53:21 -0300

    chore: remove todos os arquivos tmp e build-output da raiz

--- tmp-shopee-shorturl.py
+++
@@ -1,7 +0,0 @@
-import requests
-short_path='/opaanlp/1215229492/23694229374'
-url=f'https://shopee.com.br/api/v4/pages/is_short_url/?path={short_path}'
-headers={'User-Agent':'Mozilla/5.0','Accept':'application/json'}
-r=requests.get(url, headers=headers, timeout=30)
-print('status', r.status_code)
-print(r.text)
